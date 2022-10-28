080964ff53f06a4a58f2621941a7b0fa03e919ac
Lexsa Campbell <lexsacampbell@gmail.com>
2022-10-28 12:32:40 -0700

    Added tests (smoke, snapshot, arrow functionality)

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,6 +2,18 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+// smoke test 
+it('renders without crashing', function() {
+  render(<Carousel />);
+});
+
+// snapshot test
+it('matches snapshot', function () {
+  const {asFragment} = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+
 it("works when you click on the right arrow", function() {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
